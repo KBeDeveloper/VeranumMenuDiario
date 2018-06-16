ab0f0bcb5355013113d0ae6ba9e242b8ee0c8dca
KBeDeveloper <bengygrand@gmail.com>
2018-06-16 16:37:13 -0400

    Updated User_controller

--- application/controllers/User_controller.php
+++ application/controllers/User_controller.php
@@ -20,18 +20,26 @@
          */
         public function index()
         {
+            $this->load->model('User_model');
+            $this->load->model('Order_model');
             $user_data = [
                 "USUARIO_RUN"         => $this->input->post('input-run'),
                 "USUARIO_DV"          => $_POST['select-dv'],
                 "USUARIO_NOMBRE"      => $this->input->post('input-name'),
                 "USUARIO_TELEFONO"    => $this->input->post('input-phone'),
                 "USUARIO_CORREO"      => $this->input->post('input-email')               
-            ];            
+            ];
             $order_data = [
                 "RESERVA_COMENSALES"  => $_POST['select-commensals'],
                 "RESERVA_FECHA"       => $this->input->post('input-date'),
                 "RESERVA_HORA"        => $_POST['select-hour'],
                 "RESERVA_CLIENTE_RUN" => $this->input->post('input-run')
             ]; 
+            if (count($this->User_model->read($user_data[0]['USUARIO_RUN']))>0){
+                $this->Order_model->create($order_data);
+            }else{
+                $this->User_model->create($user_data);
+                $this->Order_model->create($order_data);
+            }
         }
 ?>
