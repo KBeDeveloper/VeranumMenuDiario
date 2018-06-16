b7873de57d2792f8b531e638d7f778b02451579f
Matias Galaz Cornejo <matiasgalaz48@gmail.com>
2018-06-16 12:50:40 -0400

    adding User_controller

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,6 +2,7 @@
 DEFINED('BASEPATH') OR EXIT('No direct script access allowed');
 
 class User_model extends CI_Model{
+    
     public function __construct(){
         parent::__construct();
         $this->load->database();        
