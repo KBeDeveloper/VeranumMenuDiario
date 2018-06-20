7cfd4279cc4144d5db027a7a3a0525de1925e574
Matias Galaz Cornejo <matiasgalaz48@gmail.com>
2018-06-19 21:06:52 -0400

    food model

---
+++ application/models/Food_model.php
@@ -0,0 +1,32 @@
+<?php 
+DEFINED('BASEPATH') OR EXIT('No direct script access allowed');
+
+class User_model extends CI_Model{
+    
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();        
+    }
+    //CRUD
+    public function create($food){
+        return $this->db->insert('COMIDA', $food);
+    }
+    public function read($food_id){
+        $this->db->where('COMIDA_ID', $food_id);
+        return $this->db->get('COMIDA')->result_array();
+    }
+    
+    public function delete($food_id){
+        $this->db->where('COMIDA_ID',$food_id);
+        return $this->db->delete('COMIDA');
+    }
+    public function getAll(){
+        return $this->db->get('COMIDA')->result_array();
+    }
+
+    public function getAllByType($food_type){
+        $this->db->where('COMIDA_TIPO',$food_type);
+        return $this->db->get('COMIDA')->result_array();
+    }
+}
+?>
