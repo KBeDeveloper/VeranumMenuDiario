9ead6bd8d490bcd452bdd77d46211fcba20a0e7d
SebCastaneda <SebCastaneda@github.com>
2018-06-20 20:22:48 +0300

    Se inserto stock para pruebas (Script en insert_meal.sql)

--- Insert_meal.sql
+++ Insert_meal.sql
@@ -9,3 +9,9 @@ VALUES
     (5,"Once Grande","Merienda Grande",3,4000),
     (6,"Salmon a la Griega","Menu optimo para compartir con su pareja",4,10006),
     (7,"Suprema grillada","Deliciosa Cena para terminar el dia con broche de oro",4,15000);
+
+INSERT INTO `stock`(`STOCK_NOMBRE`, `STOCK_CANT_DISPONIBLE`) 
+VALUES ("Huevo",10000),("Bacon",10000),("Pan",10000),("Cereal",10000),
+       ("Pasta",10000),("Carne",20000),("Cebolla",5000),("Tomate",9000),
+       ("Te",3000),("Jamon",5940),("Waffles",5000),("Salmon",3500),
+       ("Arroz",5000),("Sopa",4000);
