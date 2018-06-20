54b47ce668a3ae8ed5cb0e4d3dacf1b5d3b8b16e
Matias Galaz Cornejo <matiasgalaz48@gmail.com>
2018-06-19 22:36:42 -0400

    Menu_controller 3.0

--- application/controllers/Menu_controller.php
+++ application/controllers/Menu_controller.php
@@ -7,11 +7,11 @@ class Menu_controller extends CI_Controller {
 		header('Access-Control-Allow-Origin: http://127.0.0.1/');
 		header('Access-Control-Allow-Origin: htpp://localhost/');
 		header('Access-Control-Allow-Credentials: true');
-		$this->load->model('Meal_model');
-		$listaD['listaD']=$this->Meal_model->getAllByType(1);
-		$listaA['listaA']=$this->Meal_model->getAllByType(2);
-		$listaC['listC']=$this->Meal_model->getAllByType(3);
-		$listaO['listaO']=$this->Meal_model->getAllByType(4);
-		$this->load->view('menu_view',$listaD,$listaA,$listaC,$listaO);		
+		$this->load->model('Meal_model');		
+		$listaM['listaD']=$this->Meal_model->getAllByType(1);
+		$listaM['listaA']=$this->Meal_model->getAllByType(2);
+		$listaM['listaC']=$this->Meal_model->getAllByType(3);
+		$listaM['listaO']=$this->Meal_model->getAllByType(4);				
+		$this->load->view('menu_view',$listaM);		
 	}
 }
