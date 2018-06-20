f3e23a64be5b0aec213df8ac14bf6d577e873e61
Matias Galaz Cornejo <matiasgalaz48@gmail.com>
2018-06-19 21:08:48 -0400

    food model 2.0

--- application/models/Food_model.php
+++ application/models/Food_model.php
@@ -15,6 +15,10 @@ class User_model extends CI_Model{
         $this->db->where('COMIDA_ID', $food_id);
         return $this->db->get('COMIDA')->result_array();
     }
+    public function update($food_id, $food_data){
+        $this->db->where('COMIDA_ID', $food_id);
+        return $this->db->update('COMIDA', $food_data);
+    }
     
     public function delete($food_id){
         $this->db->where('COMIDA_ID',$food_id);
